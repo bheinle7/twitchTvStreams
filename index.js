491c2570004c34b49a9ac8863ebff38505587d40
Benjamin Heinle <bheinle7@gmail.com>
2017-06-22 17:47:45 -0500

    data from JSON is organized into relavant arrays that can be will be used in Angular

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 $(document).ready(function() {
-  var routes = ["users", "channels", "streams"]
-  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-  while (channels.length > 0) {
-    for (var i = 0; i < routes.length; i++) {
-      var url = "https://wind-bow.glitch.me/twitch-api/"+routes[i]+"/"+channels[0]+"?callback=?";
-      console.log(url)
+  var i;
+  var status = [];
+  var names = [];
+  var logos = [];
+  var sites = [];
+  var routes = ["channels","streams"]
+  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"]
+  while (routes.length > 0) {
+    for (i = 0; i < channels.length; i++) {
+      var url = "https://wind-bow.glitch.me/twitch-api/" + routes[0] + "/" + channels[i] + "?callback=?";
+
+      $.getJSON(url, function(data) {
+
+        if (data.stream === null) {
+          status.push("offline");
+        } else if (data.stream) {
+          status.push("online")
+        };
+
+        if (data.display_name) {
+          names.push(data.display_name);
+        } else if (data.error) {
+          names.push(data.message);
+        };
+
+        if (data.logo) {
+          logos.push(data.logo);
+        } else if (data.error) {
+          logos.push("placeholder.jpg");
+        } else {
+          logos.push("no_logo.jpg");
+        };
+
+        if (data.url) {
+          sites.push(data.url);
+        } else if (data.error) {
+          sites.push("javascript:;")
+        } else {
+          sites.push("javascript:;")
+        };
+
+      });
     }
-    channels.shift();
+    routes.shift();
   };
-  /*var url = "https://wind-bow.glitch.me/twitch-api/users/ESL_SC2?callback=?";
-  $.getJSON(url, function (data) {
-    console.log(data);
-  });*/
 });

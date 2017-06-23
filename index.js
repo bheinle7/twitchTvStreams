bc8f83ddaaf5673febad9a581066d9dfc161a2d9
Benjamin Heinle <bheinle7@gmail.com>
2017-06-22 22:33:22 -0500

    jQuery to populate and create HTML/CSS for each of the channels. This is done so if more channel are added nothing butthe channel array has to be changed

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 $(document).ready(function() {
-  var i;
-  var status = [];
-  var names = [];
-  var logos = [];
-  var sites = [];
-  var routes = ["channels","streams"]
   var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"]
-  while (routes.length > 0) {
-    for (i = 0; i < channels.length; i++) {
-      var url = "https://wind-bow.glitch.me/twitch-api/" + routes[0] + "/" + channels[i] + "?callback=?";
+  for (var i = 0; i < channels.length; i++) {
+    var url = "https://wind-bow.glitch.me/twitch-api/channels/" + channels[i] + "?callback=?";
+    var j = 0;
+    $.getJSON(url, function(data) {
 
-      $.getJSON(url, function(data) {
 
-        if (data.stream === null) {
-          status.push("offline");
-        } else if (data.stream) {
-          status.push("online")
-        };
 
-        if (data.display_name) {
-          names.push(data.display_name);
-        } else if (data.error) {
-          names.push(data.message);
-        };
+      if (data.display_name) {
+        var name = data.display_name;
+      } else if (data.error) {
+        var name = data.message;
+      };
 
-        if (data.logo) {
-          logos.push(data.logo);
-        } else if (data.error) {
-          logos.push("placeholder.jpg");
-        } else {
-          logos.push("no_logo.jpg");
-        };
+      if (data.logo) {
+        var logo = data.logo;
+      } else if (data.error) {
+        var logo = "img/notFound.png";
+      } else {
+        var logo = "img/notFound.png";
+      };
 
-        if (data.url) {
-          sites.push(data.url);
-        } else if (data.error) {
-          sites.push("javascript:;")
-        } else {
-          sites.push("javascript:;")
-        };
+      if (data.url) {
+        site = data.url;
+      } else if (data.error) {
+        site = "javascript:;"
+      } else {
+        site = "javascript:;"
+      };
 
-      });
-    }
-    routes.shift();
+      createList(name, logo, site, j);
+      j += 1
+      console.log(j)
+
+
+    });
   };
+
+  function createList(name, logo, site, j) {
+    $("#chnls").append("<a href=" + site + " target=_blank><div id=chnl" + j + " class=row> <img class=img-size src="+logo+" alt="+name+" Logo>" + name + "</div></a>");
+    //$("#chnl"+j).css(background-image,url(logo))
+    $("#chnl" + j).addClass("borders")
+  };
+
 });
